265d1ec4b9a81e03e5a05f99832561349dbe536c
Olblak <me@olblak.com>
2019-09-24 10:56:53 +0200

    Init Release repository content

---
+++ utils/getJenkinsVersion.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python2
+
+import urllib2
+import xml.etree.ElementTree as ET
+import os
+
+
+def getJenkinsVersion(metadataUrl, version):
+    try:
+        url = metadataUrl
+        tree = ET.parse(urllib2.urlopen(url))
+        root = tree.getroot()
+
+        # Search in Maven repository for latest version of Jenkins
+        # that satisfies X.Y.Z which represents stable version
+
+        if version == 'stable':
+            versions = root.findall('versioning/versions/version')
+
+            for version in versions:
+                if len(version.text.split('.')) == 3:
+                    result = version.text
+
+            print "Latest Stable Jenkins version detected: {}".format(result)
+
+        # Search in Maven repository for latest version of Jenkins
+        # that satisfies X.Y which represents weekly version
+        elif version == 'weekly':
+            result = root.find('versioning/release').text
+            print "Latest Jenkins version detected: {}".format(result)
+
+        # In this case we assume that we provided a valid version
+        elif len(version.split('.')) > 0:
+            result = version
+            print "Jenkins version specified: {}".format(result)
+
+        else:
+            print "Something went wrong with version: {}".format(version)
+            exit(1)
+
+        return result
+
+    except urllib2.URLError as e:
+        msg = 'Something went wrong while retrieving stable version: {}'
+        print msg.format(e)
+        exit(1)
+
+
+def downloadJenkins(version):
+    downloadUrl = URL + '{}/jenkins-war-{}.war'.format(version, version)
+
+    print "Downloading version {} from {} ".format(version, downloadUrl)
+
+    try:
+        response = urllib2.urlopen(downloadUrl)
+        content = response.read()
+
+        f = open(PATH, 'w')
+        f.write(content)
+        f.close()
+        print "War downloaded to {}".format(PATH)
+
+    except urllib2.URLError as e:
+        print type(e)
+        exit(1)
+
+
+# URL = 'https://repo.jenkins-ci.org/releases/org/jenkins-ci/main/jenkins-war/'
+
+
+
+#URL = os.environ.get('JENKINS_DOWNLOAD_URL', 'https://release.repo.jenkins.io/repository/maven-releases/org/jenkins-ci/main/jenkins-war/')
+
+if 'JENKINS_PACKAGING_PROFILE' in os.environ:
+    if PACKAGING_PROFILE == 'dev':
+        DOWNLOAD_MAVEN_REPOSITORY = 'maven-releases'
+        DOWNLOAD_MAVEN_URL  = 'http://nexus/repository/'
+    elif PACKAGING_PROFILE == 'staging':
+        DOWNLOAD_MAVEN_REPOSITORY = 'olblak-sandbox'
+        DOWNLOAD_MAVEN_URL = 'https://repo.jenkins-ci.org'
+else:
+    DOWNLOAD_MAVEN_REPOSITORY = os.environ.get('DOWNLOAD_MAVEN_REPOSITORY', 'maven-releases')
+    DOWNLOAD_MAVEN_URL = os.environ.get("DOWNLOAD_MAVEN_URL", "http://nexus/repository/")
+
+URL = DOWNLOAD_MAVEN_URL + '/' + DOWNLOAD_MAVEN_REPOSITORY + '/org/jenkins-ci/main/jenkins-war/'
+
+PATH = os.environ.get('WAR', '/tmp/jenkins.war')
+VERSION = getJenkinsVersion(
+    URL + 'maven-metadata.xml',
+    os.environ.get('JENKINS_VERSION', 'weekly')
+    )
+
+def main():
+    print "VERSION: " + VERSION
+    print "Downloaded from: " + URL
+    #downloadJenkins(VERSION)
+
+
+if __name__ == "__main__":
+    main()
---
+++ utils/release.sh
@@ -0,0 +1,284 @@
+#!/bin/bash
+
+set -euxo pipefail
+
+# https://maven.apache.org/maven-release/maven-release-plugin/perform-mojo.html
+# mvn -Prelease help:active-profiles
+
+#: "${WORKSPACE:=$PWD}" # Normally defined from Jenkins environment
+
+: "${BRANCH_NAME:=experimental}"
+: "${GIT_REPOSITORY:=scm:git:git://github.com/jenkinsci/jenkins.git}"
+: "${GIT_EMAIL:=jenkins-bot@example.com}"
+: "${GIT_NAME:=jenkins-bot}"
+: "${GIT_SSH:=ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null}"
+: "${GPG_KEYNAME:=test-jenkins-release}"
+: "${GPG_FILE:=gpg-test-jenkins-release.gpg}"
+: "${JENKINS_WAR:=$WORKSPACE/war/target/jenkins.war}"
+: "${JENKINS_ASC:=$WORKSPACE/war/target/jenkins.war.asc}"
+: "${SIGN_ALIAS:=jenkins}"
+: "${SIGN_KEYSTORE:=${WORKSPACE}/jenkins.pfx}"
+: "${SIGN_CERTIFICATE:=jenkins.pem}"
+: "${MAVEN_PROFILE:=release}"
+: "${MAVEN_REPOSITORY_USERNAME:=jenkins-bot}"
+: "${MAVEN_REPOSITORY_URL:=http://nexus/repository}"
+: "${MAVEN_REPOSITORY_NAME:=maven-releases}"
+: "${MAVEN_REPOSITORY_SNAPSHOT_NAME:=maven-snapshots}"
+: "${MAVEN_PUBLIC_JENKINS_REPOSITORY_MIRROR_URL:=http://nexus/repository/jenkins-public/}"
+
+function requireRepositoryPassword(){
+  : "${MAVEN_REPOSITORY_PASSWORD:?Repository Password Missing}"
+}
+
+function requireGPGPassphrase(){
+  : "${GPG_PASSPHRASE:?GPG Passphrase Required}" # Password must be the same for gpg agent and gpg key
+}
+
+function requireKeystorePass(){
+  : "${SIGN_STOREPASS:?pass}"
+}
+
+function requireAzureKeyvaultCredentials(){
+  : "${AZURE_VAULT_CLIENT_ID:? Require AZURE_VAULT_CLIENT_ID}"
+  : "${AZURE_VAULT_CLIENT_SECRET:? Required AZURE_VAULT_CLIENT_SECRET}"
+  : "${AZURE_VAULT_TENANT_ID:? Required AZURE_VAULT_TENANT_ID}"
+}
+
+function clean(){
+    mvn -P"${MAVEN_PROFILE}" -s settings-release.xml -B  release:clean
+}
+
+function configureGit(){
+  git checkout "${BRANCH_NAME}"
+  git config --local user.email "${GIT_EMAIL}"
+  git config --local user.name "${GIT_NAME}"
+}
+
+function configureGPG(){
+  requireGPGPassphrase
+  if ! gpg --fingerprint "${GPG_KEYNAME}"; then
+    if [ ! -f "${GPG_FILE}" ]; then
+      exit "${GPG_KEYNAME} or ${GPG_FILE} cannot be found"
+    else
+      ## --pinenty-mode is needed to avoid gpg prompt during maven release
+      gpg --import --batch "${GPG_FILE}"
+    fi
+  fi
+}
+
+
+function configureKeystore(){
+  requireKeystorePass
+  if [ ! -f "${SIGN_CERTIFICATE}" ]; then
+      exit "${SIGN_CERTIFICATE} not found"
+  else
+    openssl pkcs12 -export \
+      -out "$SIGN_KEYSTORE" \
+      -in "${SIGN_CERTIFICATE}" \
+      -password "pass:$SIGN_STOREPASS" \
+      -name "$SIGN_ALIAS"
+  fi
+}
+
+function azureAccountAuth(){
+  az login --service-principal \
+    -u "$AZURE_VAULT_CLIENT_ID" \
+    -p "$AZURE_VAULT_CLIENT_SECRET" \
+    -t "$AZURE_VAULT_TENANT_ID"
+}
+
+# Download Certificate from Azure KeyVault
+function downloadAzureKeyvaultSecret(){
+  requireAzureKeyvaultCredentials
+  azureAccountAuth
+
+  az keyvault secret download \
+    --vault-name "$AZURE_VAULT_NAME" \
+    --name "$AZURE_VAULT_CERT" \
+    --file "$AZURE_VAULT_FILE"
+}
+
+function getGPGKeyFromAzure(){
+  az storage blob download \
+    --account-name "$AZURE_STORAGE_ACCOUNT" \
+    --container-name "$AZURE_STORAGE_CONTAINER_NAME" \
+    --name "$GPG_FILE" \
+    --file "$GPG_FILE"
+}
+
+function getGPGKeyFromAzure(){
+  az storage blob download \
+    --account-name "$AZURE_STORAGE_ACCOUNT" \
+    --container-name "$AZURE_STORAGE_CONTAINER_NAME" \
+    --name "$GPG_FILE" \
+    --file "$GPG_FILE"
+}
+
+function generateSettingsXml(){
+requireRepositoryPassword
+requireKeystorePass
+requireGPGPassphrase
+
+cat <<EOT> settings-release.xml
+<settings>
+  <mirrors>
+    <mirror>
+      <id>mirror-jenkins-public</id>
+      <url>${MAVEN_PUBLIC_JENKINS_REPOSITORY_MIRROR_URL}</url>
+      <mirrorOf>repo.jenkins-ci.org</mirrorOf>
+    </mirror>
+  </mirrors>
+  <profiles>
+    <profile>
+      <id>automated-release</id>
+      <activation>
+        <activeByDefault>true</activeByDefault>
+      </activation>
+      <properties>
+        <stagingRepository>${MAVEN_REPOSITORY_URL}/${MAVEN_REPOSITORY_NAME}</stagingRepository>
+        <gpg.keyname>${GPG_KEYNAME}</gpg.keyname>
+        <gpg.passphrase>${GPG_PASSPHRASE}</gpg.passphrase>
+        <jarsigner.keystore>${SIGN_KEYSTORE}</jarsigner.keystore>
+        <jarsigner.alias>${SIGN_ALIAS}</jarsigner.alias>
+        <jarsigner.storepass>${SIGN_STOREPASS}</jarsigner.storepass>
+        <jarsigner.keypass>${SIGN_STOREPASS}</jarsigner.keypass>
+      </properties>
+      <repositories>
+        <repository>
+          <id>$MAVEN_REPOSITORY_NAME</id>
+          <name>$MAVEN_REPOSITORY_NAME</name>
+          <releases>
+            <enabled>true</enabled>
+            <updatePolicy>always</updatePolicy>
+            <checksumPolicy>warn</checksumPolicy>
+          </releases>
+          <snapshots>
+            <enabled>true</enabled>
+            <updatePolicy>never</updatePolicy>
+            <checksumPolicy>fail</checksumPolicy>
+          </snapshots>
+          <url>${MAVEN_REPOSITORY_URL}/${MAVEN_REPOSITORY_NAME}/</url>
+          <layout>default</layout>
+        </repository>
+      </repositories>
+      <pluginRepositories>
+        <pluginRepository>
+          <id>$MAVEN_REPOSITORY_NAME</id>
+          <name>$MAVEN_REPOSITORY_NAME</name>
+          <releases>
+            <enabled>true</enabled>
+            <updatePolicy>always</updatePolicy>
+            <checksumPolicy>warn</checksumPolicy>
+          </releases>
+          <snapshots>
+            <enabled>true</enabled>
+            <updatePolicy>never</updatePolicy>
+            <checksumPolicy>fail</checksumPolicy>
+          </snapshots>
+          <url>${MAVEN_REPOSITORY_URL}/${MAVEN_REPOSITORY_NAME}/</url>
+          <layout>default</layout>
+        </pluginRepository>
+      </pluginRepositories>
+    </profile>
+  </profiles>
+  <servers>
+    <server>
+      <id>$MAVEN_REPOSITORY_NAME</id>
+      <username>$MAVEN_REPOSITORY_USERNAME</username>
+      <password>$MAVEN_REPOSITORY_PASSWORD</password>
+    </server>
+    <server>
+      <id>mirror-jenkins-public</id>
+      <username>$MAVEN_REPOSITORY_USERNAME</username>
+      <password>$MAVEN_REPOSITORY_PASSWORD</password>
+    </server>
+  </servers>
+  <activeProfiles>
+    <activeProfile>release</activeProfile>
+  </activeProfiles>
+</settings>
+EOT
+}
+
+function prepareRelease(){
+  requireGPGPassphrase
+  requireKeystorePass
+  printf "\\n Prepare Jenkins Release\\n\\n"
+  mvn -B release:prepare -s settings-release.xml
+}
+
+function pushCommits(){
+  : "${RELEASE_SCM_TAG:?RELEASE_SCM_TAG not definded}"
+
+  # Ensure we use ssh credentials
+  sed -i 's#url = https://github.com/#url = git@github.com:#' .git/config
+  git push origin "HEAD:$BRANCH_NAME" "$RELEASE_SCM_TAG"
+}
+
+function rollback(){
+  mvn release:rollback
+  git push --delete origin "$RELEASE_SCM_TAG"
+}
+
+function stageRelease(){
+  requireGPGPassphrase
+  requireKeystorePass
+  printf "\\n Perform Jenkins Release\\n\\n"
+  mvn -B release:stage -s settings-release.xml
+}
+
+#function promoteMavenArtifact(){
+# # http://maven.apache.org/plugins/maven-stage-plugin/copy-mojo.html
+#  mvn \
+#    stage:copy \
+#    -Dversion=$RELEASE_VERSION \
+#    -DsourceRepositoryId=XXX \
+#    -DtargetRepositoryId=YYY
+#
+#}
+
+function validateKeystore(){
+  requireKeystorePass
+  keytool -keystore "${SIGN_KEYSTORE}" -storepass "${SIGN_STOREPASS}" -list -alias "${SIGN_ALIAS}"
+}
+function verifyGPGSignature(){
+  gpg --verify "$JENKINS_ASC" "$JENKINS_WAR"
+  unzip -qc "$JENKINS_WAR" META-INF/MANIFEST.MF | grep 'Jenkins-Version' | awk '{print $2}'
+}
+
+function main(){
+  if [ $# -eq 0 ] ;then
+    configureGPG
+    configureKeystore
+    configureGit
+    validateKeystore
+    verifyGPGSignature
+    generateSettingsXml
+    prepareRelease
+    stageRelease
+  else
+    while [ $# -gt 0 ];
+    do
+      case "$1" in
+            --cleanRelease) echo "Clean Release" && generateSettingsXml && clean;;
+            --configureGPG) echo "ConfigureGPG" && configureGPG ;;
+            --configureKeystore) echo "Configure Keystore" && configureKeystore ;;
+            --configureGit) echo "Configure Git" && configureGit ;;
+            --generateSettingsXml) echo "Generate settings-release.xml" && generateSettingsXml ;;
+            --downloadAzureKeyvaultSecret) echo "Download Azure Key Vault Secret" && downloadAzureKeyvaultSecret ;;
+            --getGPGKeyFromAzure) echo "Download GPG Key from Azure" && getGPGKeyFromAzure ;;
+            --validateKeystore) echo "Validate Keystore"  && validateKeystore ;;
+            --verifyGPGSignature) echo "Verify GPG Signature" && verifyGPGSignature ;;
+            --prepareRelease) echo "Prepare Release" && generateSettingsXml && prepareRelease ;;
+            --pushCommits) echo "Push commits on $BRANCH_NAME" && pushCommits ;;
+            --rollback) echo "Rollback release $RELEASE_SCM_TAG" && rollblack ;;
+            --stageRelease) echo "Perform Release" && stageRelease ;;
+            -h) echo "help" ;;
+            -*) echo "help" ;;
+        esac
+        shift
+    done
+  fi
+}
+
+main "$@"
