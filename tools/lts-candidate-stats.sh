22275461834db7012f12b0365f2c997ccaacf232
Alexander Brandes <brandes.alexander@web.de>
2025-09-30 17:37:39 +0200

    Modernize lts-candidate-stats script (#765)
    Co-authored-by: Mark Waite <mark.earl.waite@gmail.com>

---
+++ tools/lts-candidate-stats.sh
@@ -0,0 +1,65 @@
+#!/usr/bin/env bash
+
+set -e
+
+if [ -z "$(command -v xmllint)" ]; then
+    echo "Error: xmllint is not installed. Please install it to continue." >&2
+    exit 1
+fi
+
+PREFIX="https://issues.jenkins.io/sr/jira.issueviews:searchrequest-xml/temp/SearchRequest.xml?tempMax=1000&jqlQuery="
+
+if [ "$#" -ne 1 ]; then
+    echo "Usage: ./lts-candidate-stats.sh <version>" >&2
+    exit 1
+fi
+
+label_version_dot="$1"
+
+targetVersion=$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout --batch-mode)
+if [[ "$targetVersion" != *"$version"* ]]; then
+    echo "The previous version does not appear to be released yet: $targetVersion" >&2
+    exit 1
+fi
+
+git fetch upstream master:master
+echo "Latest core version: $(git describe master --abbrev=0)"
+
+fetch_issues() {
+    local url="$1"
+    curl -s "$url" | xmllint --xpath "//*[local-name()='item']" - 2>/dev/null | \
+    awk '
+    /<item/ {initem=1; title=""; link=""}
+    /<\/item>/ {if (title && link) print "- " title " (" link ")"; initem=0}
+    initem && /<title>/ {sub(/.*<title>/,""); sub(/<\/title>.*/,""); title=$0}
+    initem && /<link>/ {sub(/.*<link>/,""); sub(/<\/link>.*/,""); link=$0}
+    '
+}
+
+fetch_postponed_candidates() {
+    local url="$1"
+    curl -s "$url" | xmllint --xpath "//*[local-name()='item']" - 2>/dev/null | \
+    awk -v rejected="${label_version_dot}-rejected" '
+    /<item/ {initem=1; title=""; link=""; label=""}
+    /<\/item>/ {
+        if (title && link && label ~ rejected)
+            print "- " title " (" link ")";
+        initem=0
+    }
+    initem && /<title>/ {sub(/.*<title>/,""); sub(/<\/title>.*/,""); title=$0}
+    initem && /<link>/ {sub(/.*<link>/,""); sub(/<\/link>.*/,""); link=$0}
+    initem && /<label>.*<\/label>/ {
+        sub(/.*<label>/,"");
+        sub(/<\/label>.*/,"");
+        if ($0 ~ rejected) {
+            label=$1;
+        }
+    }
+    '
+}
+
+echo -e "\nFixed:\n------------------"
+fetch_issues "${PREFIX}labels%3D${label_version_dot}-fixed"
+
+echo -e "\nPostponed:\n------------------"
+fetch_postponed_candidates "https://issues.jenkins.io/sr/jira.issueviews:searchrequest-xml/12146/SearchRequest-12146.xml?tempMax=1000"
