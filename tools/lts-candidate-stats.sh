489cc02fd08aaef1db6a1824936dcb137271391e
Tim Jacomb <timjacomb1@gmail.com>
2025-11-06 22:17:34 +0000

    Adapt lts-candidate-stats for Jira migration

--- tools/lts-candidate-stats.sh
+++ tools/lts-candidate-stats.sh
@@ -10,6 +10,10 @@ fi
 
 label_version_dot="$1"
 
+OWNER=jenkinsci
+REPO=jenkins
+SEARCH_LIMIT=100
+
 targetVersion=$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout --batch-mode)
 if [[ "$targetVersion" != *"$version"* ]]; then
     echo "The previous version does not appear to be released yet: $targetVersion" >&2
@@ -20,40 +24,17 @@ git fetch upstream master:master
 echo "Latest core version: $(git describe master --abbrev=0)"
 
 fetch_issues() {
-    local url="$1"
-    curl -s "$url" | \
-    awk '
-    /<item/ {initem=1; title=""; link=""}
-    /<\/item>/ {if (title && link) print "- " title " (" link ")"; initem=0}
-    initem && /<title>/ {sub(/.*<title>/,""); sub(/<\/title>.*/,""); title=$0}
-    initem && /<link>/ {sub(/.*<link>/,""); sub(/<\/link>.*/,""); link=$0}
-    '
+    gh search issues --state closed --owner $OWNER --repo $REPO --limit $SEARCH_LIMIT --label "${label_version_dot}-fixed" --json number --template 'Fixed:
+{{range .}}{{tablerow (printf "- #%v" .number | autocolor "green")}}{{end}}'
+    gh search prs --state closed --owner $OWNER --repo $REPO --limit $SEARCH_LIMIT --label "${label_version_dot}-fixed" --json number --template '{{range .}}{{tablerow (printf "- #%v" .number | autocolor "green")}}{{end}}'
 }
 
 fetch_postponed_candidates() {
-    local url="$1"
-    curl -s "$url" | \
-    awk -v rejected="${label_version_dot}-rejected" '
-    /<item/ {initem=1; title=""; link=""; label=""}
-    /<\/item>/ {
-        if (title && link && label ~ rejected)
-            print "- " title " (" link ")";
-        initem=0
-    }
-    initem && /<title>/ {sub(/.*<title>/,""); sub(/<\/title>.*/,""); title=$0}
-    initem && /<link>/ {sub(/.*<link>/,""); sub(/<\/link>.*/,""); link=$0}
-    initem && /<label>.*<\/label>/ {
-        sub(/.*<label>/,"");
-        sub(/<\/label>.*/,"");
-        if ($0 ~ rejected) {
-            label=$1;
-        }
-    }
-    '
+    gh search issues --state closed --owner $OWNER --repo $REPO --limit $SEARCH_LIMIT --label "${label_version_dot}-rejected" --json number --template 'Postponed:
+{{range .}}{{tablerow (printf "- #%v" .number | autocolor "green")}}{{end}}'
+    gh search prs --state closed --owner $OWNER --repo $REPO --limit $SEARCH_LIMIT --label "${label_version_dot}-rejected" --json number --template '{{range .}}{{tablerow (printf "- #%v" .number | autocolor "green")}}{{end}}'
 }
 
-echo -e "\nFixed:\n------------------"
-fetch_issues "https://issues.jenkins.io/sr/jira.issueviews:searchrequest-xml/temp/SearchRequest.xml?tempMax=1000&jqlQuery=labels%3D${label_version_dot}-fixed&field=title&field=link"
-
-echo -e "\nPostponed:\n------------------"
-fetch_postponed_candidates "https://issues.jenkins.io/sr/jira.issueviews:searchrequest-xml/12146/SearchRequest-12146.xml?tempMax=1000&field=title&field=link&field=labels"
+fetch_issues
+echo ""
+fetch_postponed_candidates
