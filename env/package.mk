e1e98679118a800265fcb3a95214d776001bc234
Damien Duportal <damien.duportal@gmail.com>
2025-11-06 18:47:38 +0100

    feat(packaging) use staging and then promote with all generation on local agent
    Signed-off-by: Damien Duportal <damien.duportal@gmail.com>

---
+++ env/package.mk
@@ -0,0 +1,21 @@
+#
+# Environment definition for the packaging process
+#
+
+# where to put binary files
+export BASE_BIN_DIR=/var/www/get.jenkins.io.staging
+export WARDIR=${BASE_BIN_DIR}/war${RELEASELINE}
+export MSIDIR=${BASE_BIN_DIR}/windows${RELEASELINE}
+export DEBDIR=${BASE_BIN_DIR}/debian${RELEASELINE}
+export RPMDIR=${BASE_BIN_DIR}/rpm${RELEASELINE}
+
+# where to put repository index and other web contents
+export BASE_PKG_DIR=/var/www/pkg.jenkins.io.staging
+export RPM_WEBDIR=${BASE_PKG_DIR}/rpm${RELEASELINE}
+export MSI_WEBDIR=${BASE_PKG_DIR}/windows${RELEASELINE}
+export DEB_WEBDIR=${BASE_PKG_DIR}/debian${RELEASELINE}
+
+# URL to the aforementioned webdir.
+WEBSERVER=https://pkg.jenkins.io
+export RPM_URL=${WEBSERVER}/rpm${RELEASELINE}
+export DEB_URL=${WEBSERVER}/debian${RELEASELINE}
