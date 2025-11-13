2e3d89f7bbd325dc09f93250e0b367ae443a40ac
Tim Jacomb <21194782+timja@users.noreply.github.com>
2025-11-13 23:20:20 +0000

    Apply suggestions from code review
    Co-authored-by: Hervé Le Meur <91831478+lemeurherve@users.noreply.github.com>

--- tools/lts-candidate-stats.sh
+++ tools/lts-candidate-stats.sh
@@ -24,15 +24,15 @@ git fetch upstream master:master
 echo "Latest core version: $(git describe master --abbrev=0)"
 
 fetch_issues() {
-    gh search issues --state closed --owner $OWNER --repo $REPO --limit $SEARCH_LIMIT --label "${label_version_dot}-fixed" --json number --template 'Fixed:
-{{range .}}{{tablerow (printf "- #%v" .number | autocolor "green")}}{{end}}'
-    gh search prs --state closed --owner $OWNER --repo $REPO --limit $SEARCH_LIMIT --label "${label_version_dot}-fixed" --json number --template '{{range .}}{{tablerow (printf "- #%v" .number | autocolor "green")}}{{end}}'
+    gh search issues --state closed --owner $OWNER --repo $REPO --limit $SEARCH_LIMIT --label "${label_version_dot}-fixed" --json url --template 'Fixed:
+{{range .}}{{tablerow (printf "- %v" .url | autocolor "green")}}{{end}}'
+    gh search prs --state closed --owner $OWNER --repo $REPO --limit $SEARCH_LIMIT --label "${label_version_dot}-fixed" --json url --template '{{range .}}{{tablerow (printf "- %v" .url | autocolor "green")}}{{end}}'
 }
 
 fetch_postponed_candidates() {
-    gh search issues --state closed --owner $OWNER --repo $REPO --limit $SEARCH_LIMIT --label "${label_version_dot}-rejected" --json number --template 'Postponed:
-{{range .}}{{tablerow (printf "- #%v" .number | autocolor "green")}}{{end}}'
-    gh search prs --state closed --owner $OWNER --repo $REPO --limit $SEARCH_LIMIT --label "${label_version_dot}-rejected" --json number --template '{{range .}}{{tablerow (printf "- #%v" .number | autocolor "green")}}{{end}}'
+    gh search issues --state closed --owner $OWNER --repo $REPO --limit $SEARCH_LIMIT --label "${label_version_dot}-rejected" --json url --template 'Postponed:
+{{range .}}{{tablerow (printf "- %v" .url | autocolor "green")}}{{end}}'
+    gh search prs --state closed --owner $OWNER --repo $REPO --limit $SEARCH_LIMIT --label "${label_version_dot}-rejected" --json url --template '{{range .}}{{tablerow (printf "- %v" .url | autocolor "green")}}{{end}}'
 }
 
 fetch_issues
